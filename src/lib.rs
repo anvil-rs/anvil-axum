114c688e042eb7834e28a0d59d58facfb9c231d0
sjcobb <sjc@incyan.com>
2025-02-07 09:26:37 +0000

    init flake

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,44 @@
 use askama::Template;
+use clap::{Args, ValueEnum};
 use anvil::filters;
 
-#[derive(Template)]
-#[template(path="handler.part.rs", ext="txt")]
-struct AxumHandler<'a> {
-    name: &'a str
+#[derive(Template, Args)]
+#[template(path="handler.fragment.rs", escape="none")]
+struct AxumHandler {
+    name: String 
 }
 
-#[derive(Template)]
-#[template(path="middleware.rs", ext="txt")]
-struct Middleware<'a> {
-    layer_name: &'a str,
-    middleware_name: &'a str,
+#[derive(Template, Args)]
+#[template(path="middleware.rs", escape="none")]
+struct Middleware {
+    layer_name:  String,
+    middleware_name: String,
 }
 
-#[derive(Template)]
-#[template(path="middleware_fn.rs", ext="txt")]
-struct MiddlewareFn<'a>{
-    name: &'a str
+#[derive(Template, Args)]
+#[template(path="middleware_fn.rs", escape="none")]
+struct MiddlewareFn{
+    name: String 
+}
+
+#[derive(Template, Args)]
+#[template(path="route.fragment.rs", escape="none")]
+struct Route {
+    #[arg(value_enum)]
+    method: Method,
+    path: String,
+    handler: String
+}
+
+#[derive(ValueEnum, Debug, Copy)]
+enum Method {
+    Get,
+    Post,
+    Put,
+    Delete,
+    Patch,
+    Options,
+    Head,
+    Connect,
+    Trace,
 }
